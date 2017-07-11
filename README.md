6ca28c684910f420dc97998ea63de37bd697c352
Shawn Rider <shawn@shawnrider.com>
2017-07-11 11:24:54 -0700

    Resource files finished.
    Change-Id: Icee42f556844bb5f0cfe35729b93d841b00663da

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# wats3020-image-maker
-A JavaScript exercise designed to practice DOM manipulation.
+# WATS 3020 Image Maker Project
+
+This is a JavaScript exercise designed to practice DOM manipulation by selecting
+DOM elements, creating new elements, reading attributes of elements, and then
+modifying attributes of other elements.
+
+The premise of this assignment is that we are creating an image generator like
+we find on so many sites around the web. This generator, in its base form, lets
+a user combine a background image, top text, and bottom text to create a clever
+composition. It's possible to push this image generation much further if you
+desire.
+
+## Basic Requirements
+In order to complete this project, fulfill the following requirements:
+
+    * Complete the TODOs to initialize the `ImageMaker` class in JavaScript.
+        * Select the `#image-preview` element and store it as a class attribute.
+        * Create the `topText` and `bottomText` elements and store them as class attributes.
+        * Append the `topText` and `bottomText` elements to the `#image-preview` element.
+        * Select the form fields used to modify the image.
+    * Complete the TODOs to update the `#image-preview` element using JavaScript in the `drawPreview()` method.
+        * Read the data from the form fields.
+        * Update the DOM elements with form field data to update the image preview.
+
+## Stretch Goals
+If you have fulfilled the Basic Requirements, attempt some of these stretch goals:
+
+* Add additional ways to change existing elements. For example, you could experiment with the `<input type="color">` field to allow users to change the color of the text, or you could allow a select box to choose different fonts.
+* Populate with custom images. This is not really a JS stretch, but you could populate the whole thing with custom images that serve your needs. Perhaps you want to run a viral image sharing campaign on social media? (Note: You can allow people to submit image URLs to use images from other sites, but those might not work for a variety of reasons.)
+* Enhance the presentation of the image maker form. This form is not very well styled out of the box. Improve the user experience by making this form better looking.
+* Add additional elements to the image composition. You can create additional text, bars, icons, etc. to add to the image. This involves both creating the elements in the `#image-preview` and also adding form fields to allow a user to modify the additional elements.
+* Allow the user to select the size of image to output. The `generateImage()` function in the `ImageMaker` class will accept parameters for `height` and `width`. You could allow users to change the size of the image they generate (and preview).
+
+## Credits
+The images are previewed in HTML using JavaScript to adjust styles and content.
+Users can download the image to share in other locations. Images for download
+are created using the ["dom-to-image"](https://github.com/tsayen/dom-to-image)
+module, which uses a JS `<canvas>` element to create the image file. (**NOTE**:
+This module is not covered in the project, and implementation is provided for
+you as a convenience.)
--- js/main.js
+++ js/main.js
@@ -1,10 +1,69 @@
 /* WATS 3020 Image Maker Code */
+
+//////////////////////////////////////////////////
+// ImageMaker Class Definition               /////
+////////////////////////////////////////////////////////////////////////
+// This class is used to manage the image manipulation and prep on    //
+// the page. It is instantiated when the page is loaded, and it       //
+// handles all the logic of updating the image preview with the data  //
+// submitted by users via the image maker form.                       //
+////////////////////////////////////////////////////////////////////////
+
 class ImageMaker {
     constructor(){
+        // When this class is instantiated, the `constructor()` method is executed.
+        // TODO: Set up attributes that point to the HTML elements we wish to work with.
 
+        // TODO: Select the `#image-preview` div using any document selector method.
+        this.imagePreview = ;
+
+        // TODO: create a new `<p>` element called `this.topText`
+        this.topText = ;
+        // TODO: Add a `class` attribute to `this.topText` that contains the classname "top-text".
+
+        // TODO: Append `this.topText` as a child element to `this.imagePreview`
+
+
+        // TODO: create a new `<p>` element called `this.bottomText`
+        this.bottomText = ;
+        // TODO: Add a `class` attribute to `this.bottomText` that contains the classname "bottom-text".
+
+        // TODO: Append `this.bottomText` as a child element to `this.imagePreview`
+
+        // This class also needs to use the form fields to read user input. Set
+        // those up for future use, too.
+
+        // TODO: Select the `input` element with the `name` attribute "backgroundImage"
+        this.backgroundInput = ;
+
+        // TODO: Select the `input` element with the `name` attribute "topText"
+        this.topTextInput = ;
+
+        // TODO: Select the `input` element with the `name` attribute "bottomText"
+        this.bottomTextInput = ;
+
+        // NOTE: If you add additional form fields to modify other aspects of
+        // the image, then you will need to make attributes for each of those
+        // elements here.
     }
     drawPreview(){
+        // This function is called whenever a user changes one of the form fields
+        // and whenever an image is generated for download. This function must
+        // update the style attributes and innerHTML content of the HTML
+        // elements selected in the `constructor()` of this class in order to
+        // update `this.imagePreview`.
 
+        // TODO: Update the `background-image` CSS property for `this.imagePreview`.
+
+        // TODO: Update the `innerHTML` of `this.topText`.
+
+        // TODO: Update the `innerHTML` of `this.bottomText`
+
+
+    }
+    downloadImage(){
+        this.drawPreview();
+        generateImage();
     }
 }
 
@@ -13,27 +72,40 @@ let imageMaker = new ImageMaker();
 //////////////////////////////////////////////////
 // Do Not Edit Below This Line               /////
 ////////////////////////////////////////////////////////////////////////
-let canvas = document.getElementById("canvas");
-let imageWrapper = document.getElementById("image-wrapper");
-imageWrapper.style.overflow = "hidden";
-imageWrapper.height = 0;
-imageWrapper.width = 0;
 
-function generateImage(elementID="image-preview"){
-    imageWrapper.height = 700;
-    imageWrapper.width = 1000;
-    canvas.height = 600;
-    canvas.width = 900;
-    let htmlTemplate = document.getElementById(elementID).innerHTML;
-    rasterizeHTML.drawHTML(htmlTemplate, canvas).then(function (result) {
-        console.log(result);
-    }, function (e) {
-        console.log('An error occured:', e);
-    });
+// This function uses the `domtoimage` module to render an image of the
+// `#image-preview` element and prompts the user to download the created image.
+// It is possible to use the `height` and `width` parameters to alter the size
+// of the rendered image.
+function generateImage(elementID="image-preview", height="800px", width="1280px"){
+    let htmlTemplate = document.getElementById(elementID);
+    htmlTemplate.style.height = height;
+    htmlTemplate.style.width = width;
+    let imageName = "image_" + Date.now();
+
+    // Generate image and prompt download for user.
+    domtoimage.toJpeg(htmlTemplate, { quality: 0.95 })
+        .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = imageName;
+            link.href = dataUrl;
+            link.click();
+        });
 }
 
+
+// This function creates event listeners for each every form field added to
+// the image maker form as well as the submit button that generates an image
+// for download. New form inputs can be created and will automatically have
+// a "change" listener added to them.
+//
+// The form field listeners look for a "change" event and call the
+// `imageMaker.drawPreview()` method.
+//
+// The submit listener on the form interrupts the regular form processing of the
+// browser and calls the `imageMaker.downloadImage()` method.
 function applyEventListeners(){
-    let inputs = document.querySelectorAll('input, select');
+    let inputs = document.querySelectorAll('input, select, textarea');
     for (input of inputs){
         input.addEventListener("change", function(event){
             imageMaker.drawPreview();
@@ -42,8 +114,9 @@ function applyEventListeners(){
     let imageForm = document.querySelector('form');
     imageForm.addEventListener('submit', function(event){
         event.preventDefault();
-        imageMaker.drawPreview();
-        generateImage();
+        imageMaker.downloadImage();
     })
 }
+
+// Apply event listeners on page load.
 applyEventListeners();
